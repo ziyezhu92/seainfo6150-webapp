d8acca07c54b9a4fce33ef6d1bbada649dfc435c
ziyezhu92 <ziyezhu92@gmail.com>
2019-02-06 14:07:51 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      	<Article 
-      		title = "The Statue of Liberty's torch heads to new museum"
-      		date = "November 22, 2018"
-      		author = "Kate Farley">
-      		<MarkedUpArticle/>
-      	</Article>
+      	// <Article 
+      	// 	title = "The Statue of Liberty's torch heads to new museum"
+      	// 	date = "November 22, 2018"
+      	// 	author = "Kate Farley">
+      	// 	<MarkedUpArticle/>
+      	// </Article>
         <ArticleTable articles={articles} />
       </div>
     );
